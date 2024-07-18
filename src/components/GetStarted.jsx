1088ed3c2cd2607bd2e35e527be01f80b0abf42c
Nel4Nelson <an.ekwueme@stu.unizik.edu.ng>
2024-07-18 12:22:34 +0100

    initial commit

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+
+const GetStarted = () => {
+  return (
+    <div>
+      <Button variant="destructive" className="w-full my-5">
+        Get started
+      </Button>
+    </div>
+  );
+};
+
+export default GetStarted;
